aee7efe47406293e78ac25a042e0b5d8bf6c17cc
Arpax <arpax@hotmail.it>
2020-03-28 15:06:59 +0100

    SCOPE
    Testing scoep variable

--- Week 1 Scratchpad/app.js
+++ Week 1 Scratchpad/app.js
@@ -2,9 +2,15 @@
 (function () {
 'use strict';	
 angular.module('base', [])
-.controller('baseController', function() {
+.controller('baseController', function($scope) {
 
-
+$scope.var ="A variable";
+$scope.simplefunction = function () {
+    return "Returning string";
+}
+$scope.function = function (param) {
+    return "Rerturning parameter: "+ param;
+}
 
 });
 
