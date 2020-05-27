0e3ca965397c7e518bafab3fc0a6148696b97213
Arpax <arpax@hotmail.it>
2020-05-28 00:39:38 +0200

    Formatting files

--- Week 2 Scratchpad/app.js
+++ Week 2 Scratchpad/app.js
@@ -1,68 +1,68 @@
 
 (function () {
-'use strict';	
-angular.module('base', [])
-.controller('baseController', baseController)
-.filter('alternateCase',alternateCaseFilterFactory);
-// Injection in modo elegante tramite servizio $inject
-baseController.$inject = ['$scope', '$filter','alternateCaseFilter'];
-function baseController($scope, $filter) {
-    // MORE FILTERS
-    $scope.var = "lowercase string";
-    $scope.filteredVar= $filter('uppercase')($scope.var);
-    $scope.number = 0.5;
-    // CUSTOM FILTERS
-    $scope.input = "";
-    $scope.index = 1;
+    'use strict';
+    angular.module('base', [])
+        .controller('baseController', baseController)
+        .filter('alternateCase', alternateCaseFilterFactory);
+    // Injection in modo elegante tramite servizio $inject
+    baseController.$inject = ['$scope', '$filter', 'alternateCaseFilter'];
+    function baseController($scope, $filter) {
+        // MORE FILTERS
+        $scope.var = "lowercase string";
+        $scope.filteredVar = $filter('uppercase')($scope.var);
+        $scope.number = 0.5;
+        // CUSTOM FILTERS
+        $scope.input = "";
+        $scope.index = 1;
 
-    // Watchers
-    $scope.logWatchers= function () {
-        // console.log($scope);
-        console.log("Number of wathers = "+$scope.$$watchersCount);
+        // Watchers
+        $scope.logWatchers = function () {
+            // console.log($scope);
+            console.log("Number of wathers = " + $scope.$$watchersCount);
+        };
+
+        $scope.counter = 0;
+
+        $scope.incrementOnce = function () {
+            $scope.counter = 1;
+        };
+
+        $scope.increment = function () {
+            $scope.counter++;
+        };
+        /** ADD WATCH
+        $scope.$watch('counter', function(newValue,oldValue){
+            console.log(newValue);
+            console.log(oldValue);
+        });
+         */
+
+        $scope.$watch(function () {
+            console.log("DIGEST LOOP FIRED!");
+        });
+
     };
-    
-    $scope.counter = 0;
 
-    $scope.incrementOnce = function(){
-        $scope.counter = 1;
-    };
+    function alternateCaseFilterFactory() {
+        return function (input, index) {
 
-    $scope.increment = function(){
-        $scope.counter++;
-    };
-    /** ADD WATCH
-    $scope.$watch('counter', function(newValue,oldValue){
-        console.log(newValue);
-        console.log(oldValue);
-    });
-     */
-
-    $scope.$watch(function(){
-        console.log("DIGEST LOOP FIRED!");
-    });
-
-};
-
-        function alternateCaseFilterFactory() {
-            return function(input, index) {
-                
-                var out = "";
-                for (let i = 0; i < input.length; i++) {
-                    console.log(i);
-                    if (i % index == 0) {
-                        out += input[i].toUpperCase();
-                    } else {
-                        out += input[i].toLowerCase();
-                    }
+            var out = "";
+            for (let i = 0; i < input.length; i++) {
+                console.log(i);
+                if (i % index == 0) {
+                    out += input[i].toUpperCase();
+                } else {
+                    out += input[i].toLowerCase();
                 }
-                return out;
             }
+            return out;
         }
+    }
 
     angular.module('digest', [])
         .controller('digestController', digestController);
 
-    digestController.$inject = ['$scope','$timeout'];
+    digestController.$inject = ['$scope', '$timeout'];
 
     function digestController($scope, $timeout) {
 
@@ -86,7 +86,7 @@ function baseController($scope, $filter) {
 
         $scope.applyTimeout = function () {
             setTimeout(function () {
-                    $scope.$apply(function () {
+                $scope.$apply(function () {
                     $scope.counterValue++;
                 });
             }, 2000);
