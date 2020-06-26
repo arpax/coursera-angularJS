8e7b0231852af8341b04cfbdbabb31e7a34e9914
Arpax <arpax@hotmail.it>
2020-06-26 10:02:24 +0200

    Assignment 2 part 1
    Added code to display and buy items

--- mod2_solution/app.js
+++ mod2_solution/app.js
@@ -1,6 +1,54 @@
 (function () {
 'use strict';
 
-angular.module('ShoppingListCheckOff', []);
+angular.module('ShoppingListCheckOff', [])
+.controller("ToBuyController",ToBuyController)
+.controller("AlreadyBoughtController",AlreadyBoughtController)
+.service("ShoppingListCheckOffService",ShoppingListCheckOffService);
+
+
+ToBuyController.$inject = ["ShoppingListCheckOffService"];
+function ToBuyController(ShoppingListCheckOffService) {
+
+    var tbc = this;
+
+    tbc.items = ShoppingListCheckOffService.getItemsTB();
+
+    tbc.buyItem = function (itemIndex) {
+      ShoppingListCheckOffService.buy(itemIndex);
+    }
+}
+
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+
+  var abc = this;
+
+  abc.items = ShoppingListCheckOffService.getItemsAB();
+
+}
+
+  // Service that manages the shopping list -- BUSINESS LOGIC
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    var itemsToBuy = ["10 Chocolate Cookies", "2 Vanilla Cookies", "8 Butter Cookies"];
+    var itemsBought = [];
+
+    //EXPOSED METHOD TO MOVE ITEMS
+    service.buy = function (index) {
+      itemsBought.push(itemsToBuy.splice(index, 1)[0]);
+    }
+
+    //EXPOSED METHOD TO GET ITEMS
+    service.getItemsTB = function () {
+      return itemsToBuy;
+    }
+
+    //EXPOSED METHOD TO GET ITEMS
+    service.getItemsAB = function () {
+      return itemsBought;
+    }
+
+  }
 
 })();
