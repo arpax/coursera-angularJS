e6de4f110a9e74791a4a06afe61d4747db7535e0
Arpax <arpax@hotmail.it>
2020-06-27 21:14:05 +0200

    Promises imlementation
    Added code to validate items before adding

--- Week 3 Scratchpad/app.js
+++ Week 3 Scratchpad/app.js
@@ -3,9 +3,10 @@
     'use strict';
 
 
-    angular.module('ShoppingListPromiseApp',[])
+    angular.module('ShoppingListPromiseApp', [])
         .controller('ShoppingListController', ShoppingListController)
-        .service('ShoppingListService', ShoppingListService);
+        .service('ShoppingListService', ShoppingListService)
+        .service('ValidationService', ValidationService)
 
 
     ShoppingListController.$inject = ['ShoppingListService'];
@@ -15,39 +16,167 @@
         list.itemName = "";
         list.itemQuantity = "";
 
-        list.addItem = function () {
-            shoppingListService.addItem(list.itemName, list.itemQuantity)
+        list.addItemCallback = function () {
+            shoppingListService.addItemCallback(list.itemName, list.itemQuantity)
+            list.itemName = "";
+            list.itemQuantity = "";
+        }
+
+        list.addItem2 = function () {
+            shoppingListService.addItem2(list.itemName, list.itemQuantity)
+            list.itemName = "";
+            list.itemQuantity = "";
+        }
+
+        list.addItem3 = function () {
+            shoppingListService.addItem3(list.itemName, list.itemQuantity)
             list.itemName = "";
             list.itemQuantity = "";
         }
 
         list.items = shoppingListService.getItems();
 
-        list.removeItem = function (itemIndex){
+        list.removeItem = function (itemIndex) {
             shoppingListService.removeItem(itemIndex);
         }
 
     }
 
-
-    function ShoppingListService() {
+    ShoppingListService.$inject = ["$q", "ValidationService"];
+    function ShoppingListService($q, validationService) {
 
         var service = this;
 
         var itemList = [];
 
-        service.addItem = function (name, quantity) {
-            var dummyItem = { "name": name, "quantity": quantity }
-            itemList.push(dummyItem);
+        // Calling the validation service with callbacks
+        service.addItemCallback = function (name, quantity) {
+
+            var promise = validationService.checkName(name); //THIS RETURNS A PROMISE OBJECT
+
+            // Execute the name check and manage response
+            // First argument --> promise resolved
+            // Second argument --> promise rejected
+            promise.then(function (response) {
+
+                var nextPromise = validationService.checkQuantity(quantity); //THIS RETURNS A PROMISE OBJECT
+
+                // Execute the quantity check and manage response.
+                // First argument --> promise resolved
+                // Second argument --> promise rejected
+                nextPromise.then(function (nextResponse) {
+
+                    // Both Validations passed
+                    var dummyItem = { "name": name, "quantity": quantity }
+                    itemList.push(dummyItem);
+
+                }, function (nextResponse) {
+                    // Quantity validation not passed
+                    console.log(nextResponse.message);
+                });
+
+            }, function (response) {
+                // Name validation not passed
+                console.log(response.message);
+
+            });
+
         }
+
+        // Calling the validation service chaining promises
+        service.addItem2 = function (name, quantity) {
+
+            var promise = validationService.checkName(name);
+
+            promise.then(function (response) {
+                return validationService.checkQuantity(quantity);
+            }).then(function (response) {
+
+                // Validation (both) passed
+                var dummyItem = { "name": name, "quantity": quantity }
+                itemList.push(dummyItem);
+
+            }).catch(function (response) {
+                // Validation (any) not passed
+                console.log(response.message);
+            });
+
+
+        }
+
+        service.addItem3 = function (name, quantity) {
+
+            // Calling all the validation services
+            $q.all([validationService.checkName(name), validationService.checkQuantity(quantity)])
+                .then(function (response) {
+
+                    // Validation passed
+                    var dummyItem = { "name": name, "quantity": quantity }
+                    itemList.push(dummyItem);
+
+                }).catch(function (response) {
+                    // Validation (any) not passed
+                    console.log(response.message);
+                });
+
+        }
+
+
+
 
         service.getItems = function () {
             return itemList;
         }
 
-        service.removeItem = function(itemIndex){
-            itemList.splice(itemIndex,1);
+        service.removeItem = function (itemIndex) {
+            itemList.splice(itemIndex, 1);
         }
+    }
+
+    // Validation services
+    ValidationService.$inject = ["$q", "$timeout"]; // injecting the $q service (promise API)
+    function ValidationService($q, $timeout) {
+
+        var validation = this;
+
+        validation.checkName = function (name) {
+            var deferred = $q.defer(); // Returns an API object
+            var result = { message: "" };
+
+            $timeout(function () {
+                if (name.toLowerCase().indexOf("cookie") === -1) {
+                    deferred.resolve(result); // Promise is successfully resovled
+                } else {
+                    result.message = "cookies not allowed"
+                    deferred.reject(result); // Promise is rejected (error)
+                }
+
+
+            }, 3000);
+
+            return deferred.promise; // Returns promise
+
+        };
+
+        validation.checkQuantity = function (quantity) {
+            var deferred = $q.defer();
+            var result = { message: "" };
+
+            $timeout(function () {
+                if (quantity <= 5) {
+                    deferred.resolve(result);
+                } else {
+                    result.message = "maximum 5 boxes"
+                    deferred.reject(result);
+                }
+
+            }, 1000);
+
+            return deferred.promise;
+
+        }
+
+
     }
 
 
