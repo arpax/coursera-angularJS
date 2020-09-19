5ba1f16e4a9de5808ece653100f95923c5f88cdd
Arpax <arpax@hotmail.it>
2020-09-19 11:51:03 +0200

    Assignment 4 ui router setup

--- mod4_solution/app.js
+++ mod4_solution/app.js
@@ -1,6 +1,27 @@
 (function(){
 
 'use strict';
-angular.module('restaurantApp',[])
+angular.module('restaurantApp',['ui.router'])
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to tab 1 if no other URL matches
+  $urlRouterProvider.otherwise('/home');
+
+  // Set up UI states
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'src/home.html'
+    })
+
+    .state('tab2', {
+      url: '/tab2',
+      templateUrl: 'src/tab2.html'
+    });
+}
+
 
 })();
